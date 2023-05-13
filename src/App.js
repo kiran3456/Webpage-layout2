bd1b5c570168a3973102fa12b52d07a2d82454d2
Kiran <kirandeepkaurfdk5@gmail.com>
2023-05-12 18:11:02 -0800

    layout2 working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containerV">
+      <div className="boxV boxV1">Box 1</div>
+      <div className="boxV boxV2">
+        <div className="containerH">
+          <div className="boxH boxH1">Box 1</div>
+          <div className="boxH boxH2">Box 2</div>
+          <div className="boxH boxH3">Box 3</div>
+        </div>
+      </div>
+      <div className="boxV boxV3">Box 3</div>
     </div>
   );
 }
